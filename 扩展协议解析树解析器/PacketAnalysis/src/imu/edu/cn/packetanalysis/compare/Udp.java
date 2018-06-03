024b25058e6d8ea1eb59d81d9f55503b6f37c1a7
huxinlei <huxinlei2010@163.com>
2018-06-03 16:36:40 +0800

    提交失败，重新提交

--- 扩展协议解析树解析器/PacketAnalysis/src/imu/edu/cn/packetanalysis/compare/Udp.java
+++
@@ -1,8 +0,0 @@
-package imu.edu.cn.packetanalysis.compare;
-
-public class Udp {
-	public static int sourceport = 16;//1
-	public static int destport = 16;//2
-	public static int length = 16;//3
-	public static int checksum = 16;//4
-}
